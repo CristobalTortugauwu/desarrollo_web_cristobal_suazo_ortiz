bb29fd1fd924a7f0545e32da324ada248461a6a9
CristobalTortugauwu <cristobal.suazo@ug.uchile.cl>
2025-09-05 17:11:42 -0400

    ahora se puede navegar correctamente entre listado y gatos_k k={1,2,3,4,5}

--- listado_adopciones.js
+++ listado_adopciones.js
@@ -13,3 +13,17 @@ const movetonewpage4 = () => {
 const movetonewpage5 = () => {
     window.location.href="contenido/hojas_imagenes/gato_5.html";
 }
+
+const volver_a_listado = () => {
+    window.location.href="../../ListadoAdopciones.html";
+}
+
+const volver_al_inicio = () => {
+    window.location.href="../../view.html";
+}
+
+
+var BotonVolverAtras = document.getElementById("volver-listado-btn");
+var BotonVolverInicio = document.getElementById("volver-inicio-btn");
+BotonVolverAtras.addEventListener("click",volver_a_listado);
+BotonVolverInicio.addEventListener("click",volver_al_inicio);
